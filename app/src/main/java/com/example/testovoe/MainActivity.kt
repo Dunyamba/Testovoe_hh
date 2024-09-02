aaf74cfd5f20ef1dafc1c1a8c4ddd951613ea82d
Dunkan <zanku1996@gmail.com>
2024-09-03 00:44:43 +0300

    first settings

--- app/src/main/java/com/example/testovoe/MainActivity.kt
+++ app/src/main/java/com/example/testovoe/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.testovoe
 
 import android.os.Bundle
+import android.view.View
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
@@ -18,7 +19,7 @@ class MainActivity : AppCompatActivity() {
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
+        supportActionBar?.hide()
         val navView: BottomNavigationView = binding.navView
 
         val navController = findNavController(R.id.nav_host_fragment_activity_main)
